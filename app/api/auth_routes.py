7f7db420cfb8a83072628dd76621d6857cc62da6
Jefferson <jeffersongarcia1599@gmail.com>
2021-07-26 06:43:26 -0500

    SignUp working and aws successfully adds pictures

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -62,30 +62,13 @@ def sign_up():
     """
     form = SignUpForm()
     form['csrf_token'].data = request.cookies['csrf_token']
-    
-    if "image" not in request.files:
-        return {'errors': ['image required']}, 400
 
-    image = request.files["image"]
-
-    if not allowed_file(image.filename):
-        return {"errors": ["file type not permitted"]}, 400
-
-    image.filename = get_unique_filename(image.filename)
-
-    upload = upload_file_to_s3(image)
-
-    if "url" not in upload:
-        return {'errors': [upload]}, 400
-
-    url = upload["url"]
     if form.validate_on_submit():
         user = User(
             username=form.data['username'],
             full_name =form.data['full_name'],
             email=form.data['email'],
             password=form.data['password'],
-            profile_image=url,
         )
         db.session.add(user)
         db.session.commit()
