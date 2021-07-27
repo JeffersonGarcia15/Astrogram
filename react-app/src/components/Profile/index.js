3317e0e7525e1be3153ae73da0e37fbf4dfc9ce6
Jefferson <jeffersongarcia1599@gmail.com>
2021-07-27 06:34:19 -0500

    Got only one single profile picture displaying as intended

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -1,29 +1,58 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, useHistory, useParams } from 'react-router-dom';
-import { getOwnPosts } from '../../store/post';
+import { getOwnPosts, getAllPosts } from '../../store/post';
+import Grid from '@material-ui/core/Grid'
 
 
 
 function Profile() {
     const history = useHistory()
     const dispatch = useDispatch()
+    const { userId } = useParams()
     const user = useSelector(state => state.session.user)
     const posts = useSelector(state => state.posts)
-    // const id = user?.id
-    const { userId } = useParams()
-    
+    const postInfo = Object.values(posts)
+
+    // useEffect(() => {
+    //     dispatch(getOwnPosts(userId))
+    // }, [dispatch, userId])
     useEffect(() => {
-        dispatch(getOwnPosts(userId))
-    }, [dispatch, userId])
+        dispatch(getAllPosts())
+    }, [dispatch])
 
     console.log('POOOOOOOST FOR USER', posts);
-    // console.log('USERID', userId);
-    // console.log('USER.ID', userId);
+    console.log('postInfo#############', postInfo);
+    console.log('MAYBE USE USER?', user);
 
     return (
         <div>
-            
+            <Grid container align="center">
+                <Grid item md={3}></Grid>
+                <Grid item md={6}>
+                    <div>
+                        <img src={user.profile_image} />
+                    </div>
+                    {Object.values(posts)?.map(post => {
+                        return (
+                        // <div key={post?.id}>
+                        //     <img src={post?.user.profile_image} />
+                        // </div>
+                        <>
+                            {post.user.id == userId && (
+                                <div key={post?.id}>
+                                    <div>
+                                        {/* <img src={post?.user.profile_image} /> */}
+                                    </div>
+                                </div>
+                            )}
+                        
+                        </>
+                        )
+                })}
+                </Grid>
+                <Grid item md={3}></Grid>
+            </Grid>
         </div>
     )
 }
