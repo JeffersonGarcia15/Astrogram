a7f67072668404836bf0fa3458e31d1ee4897c80
Jefferson <jeffersongarcia1599@gmail.com>
2021-07-29 01:04:43 -0500

    Fixed issue on comments

--- react-app/src/store/comment.js
+++ react-app/src/store/comment.js
@@ -26,7 +26,7 @@ const deleteSingleComment = comment => ({
 
 
 export const getAllComments = () => async (dispatch) => {
-    const response = await fetch("/api/comments")
+    const response = await fetch("/api/comments/")
 
     if (response.ok) {
         const comments = await response.json()
