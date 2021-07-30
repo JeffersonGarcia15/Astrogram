8ab4294acfc2c63fd01a0e4642e3e1ecf319c6a1
Jefferson <jeffersongarcia1599@gmail.com>
2021-07-30 12:08:45 -0500

    like routes working test using postman

--- app/api/likes_routes.py
+++ app/api/likes_routes.py
@@ -11,3 +11,23 @@ def likes(id):
         "likes": [like.to_dict() for like in likes]
     }
 
+
+@like_routes.route('/new', methods=['POST'])
+def postLike():
+    request_json = request.get_json()
+    like = Like(
+        user_id=request_json['user_id'],
+        post_id=request_json['post_id']
+    )
+    db.session.add(like)
+    db.session.commit()
+    return request.get_json()
+
+@like_routes.route('/<int:id>', methods=['DELETE'])
+def deleteLike(id):
+    delete_like = Like.query.get(id)
+    db.session.delete(delete_like)
+    db.session.commit()
+    return {
+        "delete_like": delete_like.to_dict()
+    }
