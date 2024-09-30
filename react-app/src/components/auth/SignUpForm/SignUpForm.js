3542f60024616a822a46b932eb5b25875c6bd95b
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-09-30 13:17:38 -0500

    Google-Log-In/jajg Fixing sign up.

--- react-app/src/components/auth/SignUpForm/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm/SignUpForm.js
@@ -1,88 +1,102 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect, useHistory } from 'react-router-dom';
-import { signUp, signUpGoogle } from '../../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect, useHistory } from "react-router-dom";
+import { signUp, signUpGoogle } from "../../../store/session";
 
-import ValidateEmail from '../../utils'
-import { GoogleLogin } from 'react-google-login'
-import Demo from '../../Demo';
-import '../auth.css'
+import ValidateEmail from "../../utils";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import Demo from "../../Demo";
+import "../auth.css";
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [full_name, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [username, setUsername] = useState("");
+  const [full_name, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
-  const history = useHistory()
+  const history = useHistory();
 
   const onSignUp = async (e) => {
     e.preventDefault();
-    setErrors([])
+    setErrors([]);
 
-    let validatorErrors = []
+    let validatorErrors = [];
 
     if (!ValidateEmail(email)) {
-      validatorErrors.push('Please provide a valid email address')
+      validatorErrors.push("Please provide a valid email address");
     }
 
     if (full_name.length < 3) {
-      validatorErrors.push('Please provide a valid full name with more than 3 characters')
-    }
-    else if (username.length > 50) {
-      validatorErrors.push('Please provide a valid full name with not more than 50 characters')
+      validatorErrors.push(
+        "Please provide a valid full name with more than 3 characters"
+      );
+    } else if (username.length > 50) {
+      validatorErrors.push(
+        "Please provide a valid full name with not more than 50 characters"
+      );
     }
 
     if (password.length < 6) {
-      validatorErrors.push('Please provide a password with 6 or more characters')
+      validatorErrors.push(
+        "Please provide a password with 6 or more characters"
+      );
     }
 
     if (password.length > 15) {
-      validatorErrors.push('Please provide a password not longer than 15 characters')
+      validatorErrors.push(
+        "Please provide a password not longer than 15 characters"
+      );
     }
 
     if (username.length < 3) {
-      validatorErrors.push('Please provide a username with at least 3 characters')
-    }
-    else if (username.length > 15) {
-      validatorErrors.push('Please provide a username not longer than 15 characters')
+      validatorErrors.push(
+        "Please provide a username with at least 3 characters"
+      );
+    } else if (username.length > 15) {
+      validatorErrors.push(
+        "Please provide a username not longer than 15 characters"
+      );
     }
 
     if (password !== repeatPassword) {
-      validatorErrors.push('Passwords do not match')
+      validatorErrors.push("Passwords do not match");
     }
 
     if (!validatorErrors.length) {
       const data = await dispatch(signUp(username, full_name, email, password));
       if (data?.errors) {
-        setErrors(data?.errors)
-
-      }
-      else {
-
+        setErrors(data?.errors);
+      } else {
         setTimeout(() => {
-          history.push('/feed')
-        }, 0)
+          history.push("/feed");
+        }, 0);
       }
-
-    }
-    else {
-      setErrors(validatorErrors)
+    } else {
+      setErrors(validatorErrors);
     }
   };
   const responseGoogle = async (response) => {
-    const values = await dispatch(signUpGoogle({username: response?.Rs?.mU, full_name: response?.Rs?.Qe, email: response?.Rs?.Ct, password: 'password1!'}));
+    const decodedToken = jwtDecode(response.credential);
+    console.log("Decoded Token", decodedToken);
+    const values = await dispatch(
+      signUpGoogle({
+        username: decodedToken.name,
+        full_name: decodedToken.name,
+        email: decodedToken.email,
+        password: "password1!",
+      })
+    );
+    console.log("THE VALUE", values);
     if (user) {
-      history.push('/feed')
-
+      history.push("/feed");
     }
 
-
-    return response
-  }
+    return response;
+  };
 
   const updateUsername = (e) => {
     setUsername(e.target.value);
@@ -100,25 +114,34 @@ const SignUpForm = () => {
     setRepeatPassword(e.target.value);
   };
 
-
   function FloatingEvt(evt) {
     if (evt.target.value.length > 0) {
-      evt.target.classList.add('has-value')
+      evt.target.classList.add("has-value");
     } else {
-      evt.target.classList.remove('has-value')
+      evt.target.classList.remove("has-value");
     }
   }
 
   if (user) {
-    return <Redirect to='/feed' />;
+    return <Redirect to="/feed" />;
   }
 
   return (
     <div className="container">
-      <img src={'https://astrogram.s3.us-east-2.amazonaws.com/Screen+Shot+2021-07-04+at+6.30.15+PM.png'} alt='logo'></img>
-      <div className='form-group'>
+      <img
+        src={
+          "https://astrogram.s3.us-east-2.amazonaws.com/Screen+Shot+2021-07-04+at+6.30.15+PM.png"
+        }
+        alt="logo"
+      ></img>
+      <div className="form-group">
         <div className="form-container">
-          <img className="logo" src="https://i.ibb.co/pWpLBFN/Astrogram.png" alt="Astrogram" border="0" />
+          <img
+            className="logo"
+            src="https://i.ibb.co/pWpLBFN/Astrogram.png"
+            alt="Astrogram"
+            border="0"
+          />
           <p className="title">
             Sign up to see photos and videos from your friends.
           </p>
@@ -128,26 +151,28 @@ const SignUpForm = () => {
             <p className="t-line">OR</p>
             <p className="r-line"></p>
           </div>
-          <p style={{ textAlign: 'center' }}>Login with Google</p>
           <GoogleLogin
-            clientId="60469361499-a3opbb8lsiqp1h4b4p68nfksn125lqaa.apps.googleusercontent.com"
             buttonText="Login with Google"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-            onClick={e => { e.preventDefault(); history.push(`/feed`) }}
-
+            cookiePolicy={"single_host_origin"}
+            // onClick={(e) => {
+            //   e.preventDefault();
+            //   history.push(`/feed`);
+            // }}
           ></GoogleLogin>
           <form onSubmit={onSignUp}>
             <div>
               {errors.map((error, ind) => (
-                <div className="error-container" key={ind}>{error}</div>
+                <div className="error-container" key={ind}>
+                  {error}
+                </div>
               ))}
             </div>
             <div className="floating-label">
               <input
-                type='text'
-                name='email'
+                type="text"
+                name="email"
                 className="form-control"
                 onChange={updateEmail}
                 value={email}
@@ -158,8 +183,8 @@ const SignUpForm = () => {
             </div>
             <div className="floating-label">
               <input
-                type='text'
-                name='fullName'
+                type="text"
+                name="fullName"
                 onChange={(e) => setFullName(e.target.value)}
                 value={full_name}
                 className="form-control"
@@ -170,8 +195,8 @@ const SignUpForm = () => {
             </div>
             <div className="floating-label">
               <input
-                type='text'
-                name='username'
+                type="text"
+                name="username"
                 onChange={updateUsername}
                 value={username}
                 className="form-control"
@@ -182,8 +207,8 @@ const SignUpForm = () => {
             </div>
             <div className="floating-label">
               <input
-                type='password'
-                name='password'
+                type="password"
+                name="password"
                 onChange={updatePassword}
                 value={password}
                 className="form-control"
@@ -194,8 +219,8 @@ const SignUpForm = () => {
             </div>
             <div className="floating-label">
               <input
-                type='password'
-                name='repeat_password'
+                type="password"
+                name="repeat_password"
                 onChange={updateRepeatPassword}
                 value={repeatPassword}
                 required={true}
@@ -205,12 +230,23 @@ const SignUpForm = () => {
               ></input>
               <label>Repeat Password</label>
             </div>
-            <button type='submit' className="btn-form">Sign Up</button>
+            <button type="submit" className="btn-form">
+              Sign Up
+            </button>
           </form>
         </div>
         <div className="form-bottom">
-          <p>Have an account?
-            <a onClick={e => { e.preventDefault(); history.push(`/login`) }} href="/login">Login</a>
+          <p>
+            Have an account?
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                history.push(`/login`);
+              }}
+              href="/login"
+            >
+              Login
+            </a>
           </p>
         </div>
       </div>
